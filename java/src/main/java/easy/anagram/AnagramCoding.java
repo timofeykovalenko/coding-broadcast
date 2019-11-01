a862c56306400cefef347f117db7a2893f19c67a
timofeykovalenko <>
2019-11-01 16:51:42 +0300

    Add contract for anagram problem

---
+++ java/src/main/java/easy/anagram/AnagramCoding.java
@@ -0,0 +1,11 @@
+package easy.anagram;
+
+class AnagramCoding implements Anagram {
+
+    public boolean isAnagram(String textOne, String textTwo) {
+
+        // TODO Implement
+
+        return false;
+    }
+}
